525b597868581d785779a4de4e25a3bfb78d1b48
Oliver Galante <oliverandcarli@gmail.com>
2023-10-30 17:16:49 -0400

    Added Id to Profile and Post to easily track who is creating the Post and which Porfile it is associated with.

--- Blog_app/serializers.py
+++ Blog_app/serializers.py
@@ -5,10 +5,10 @@ from .models import Post, Profile
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['title', 'content', 'author', 'created_at']
+        fields = ['id', 'title', 'content', 'author', 'created_at']
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['user']
+        fields = ['id', 'user']
